ad2f14f81d352caa030da72df1452997e596833d
flambuth <flambuth@gmail.com>
2024-03-20 09:39:31 -0500

    weekly dash img and url fixes

--- app/dash_plotlys/dash_apps/big_dash.py
+++ app/dash_plotlys/dash_apps/big_dash.py
@@ -3,6 +3,8 @@ from dash import dcc
 from dash import html
 from dash import Input, Output
 
+from urllib import parse
+
 import dash_bootstrap_components as dbc
 from dash_bootstrap_templates import load_figure_template
 load_figure_template('VAPOR')
@@ -65,7 +67,7 @@ def Add_Big_Dash(flask_app):
             html.Div(id='top-subgenre-card', style={'padding':'10px'}),
             html.Div(id='top-master-genre-card', style={'padding':'10px'}),
             html.Div(id='top-song-card', style={'padding':'10px'}),
-        ], width=12, lg=3, className="dash-div"),
+        ], width=12, lg=2, className="dash-div"),
         dbc.Col([
             # 1 line graph
             dbc.Row([
@@ -91,25 +93,25 @@ def Add_Big_Dash(flask_app):
                     lg=8,
                 ),
             ],  style={'margin-bottom': '20px', 'padding': '10px'}),
-        ], width=12, lg=9),
+        ], width=12, lg=10),
     ]),
 
     # cards row
     dbc.Row([
-        dbc.Col(html.Div(id='top-3-cards'), style={'padding': '10px'}),  
+        dbc.Col(html.Div(id='top-5-cards'), style={'padding': '10px'}),  
     ], className="dash-div", style={'padding': '10px'}),
     layouts.my_icon,
     ], fluid=True,)
 
 
-    dash_app.title = 'Every Whizbang Plotly Can Muster'
+    dash_app.title = "The Past Few Weeks Or So"
 
     #the component_ids are referenced in the dash_app.layout. There are dcc or html objects that have 
     #the same id as the component ids in this dash callback.
     @dash_app.callback(
         Output('day-of-week-graph', 'figure'),
         Output('known-pie-graph', 'figure'),
-        Output('top-3-cards', 'children'),
+        Output('top-5-cards', 'children'),
         Output('date-range-info', 'children'),
         Output('hourly-line-graph', 'figure'),
         Output('top-subgenre-card', 'children'),
@@ -118,32 +120,48 @@ def Add_Big_Dash(flask_app):
         Input('daterange-selection', 'value'),        
     )
     def update_graph(value):
+        '''
+        Value is the amount of days. The radio buttons in the layout give 7, 14, or 21
+        5 is the amount of artists to put in the bottom row of top artists
+        Fred_Big_Dash_Stuff holds attributes and methods to fill each Output in the callback
+        '''
         rp_stuff = data_sources.Fred_Big_Dash_Stuff(int(value), 5)
-        top_3_imgs_div = layouts.top_3_imgs(rp_stuff.top_tuples)
+
+        #the row at the bottom
+        top_5_imgs_div = layouts.top_artists_imgs(rp_stuff.top_tuples)
 
         day_of_week_fig = plotly_figures.day_of_week_bars(rp_stuff.dts, rp_stuff.mean_song_per_day)
         known_pie_fig = plotly_figures.un_known_pie_chart(rp_stuff.known, rp_stuff.unknown)
 
         hourly_line_fig = plotly_figures.hourly_listening_line_chart(rp_stuff.avg_song_per_hour_Series())
 
+        #date range in headline
         date_range_info = html.H5(f'{rp_stuff.first_day} thru {rp_stuff.last_day}')  # New date range info
 
+        #side card column
         top_subgenre_card = layouts.side_card(
-            'Top Sub-Genre',
-            rp_stuff.rando_subgenre_ac.img_url_mid,
-            rp_stuff.known_genre_counts()[1][0][0]
+            'Top Subgenre',
+            rp_stuff.rando_subgenre_ac.img_url,
+            rp_stuff.known_genre_counts()[1][0][0],
+            f"https://fredlambuth.com/spotify/art_cat/genre/{rp_stuff.known_genre_counts()[1][0][0]}",
+            rp_stuff.rando_subgenre_ac.art_name,
+            
         )
         top_master_genre_card = layouts.side_card(
             'Top Genre',
             rp_stuff.rando_master_genre_ac.img_url,
-            rp_stuff.rando_master_genre_ac.master_genre
+            rp_stuff.rando_master_genre_ac.master_genre,
+            f"https://fredlambuth.com/spotify/art_cat/genre/{rp_stuff.rando_master_genre_ac.master_genre}",
+            rp_stuff.rando_master_genre_ac.art_name,
         )
         top_song_card = layouts.side_card(
             'Top Song',
             rp_stuff.top_rp.image[-40:],
-            rp_stuff.top_rp.song_name
+            rp_stuff.top_rp.song_name,
+            rp_stuff.top_rp.song_link,
+            rp_stuff.top_rp.art_name,
         )
 
-        return day_of_week_fig, known_pie_fig, top_3_imgs_div, date_range_info, hourly_line_fig, top_subgenre_card, top_master_genre_card, top_song_card
+        return day_of_week_fig, known_pie_fig, top_5_imgs_div, date_range_info, hourly_line_fig, top_subgenre_card, top_master_genre_card, top_song_card
 
     return dash_app.server
